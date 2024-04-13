412bcbecf0ec39f03e7605f0133065aa06722821
dariNirvana <takina0721@gmail.com>
2024-04-14 02:18:05 +0800

    fix env path error

--- bot/env.py
+++ bot/env.py
@@ -1,10 +1,9 @@
 import os
 
 # optional
-PROXY_URL = os.getenv('PROXY_URL', str)  # eg. 'http://localhost:port' and 'http://' is required
-DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', int)  # recommend at 8-16
+PROXY_URL = os.getenv('PROXY_URL', None)  # eg. 'http://localhost:port' and 'http://' is required
+DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', 4)  # recommend at 8-16
 # necessary
-BOT_TOKEN = os.getenv('TG_BOT_TOKEN', str)  # get from bot father
-DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', str)
+BOT_TOKEN = os.getenv('BOT_TOKEN', '')  # get from bot father
 USER_ID = os.getenv('USER_ID', int)
-
+DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', '/download')
