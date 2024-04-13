b1365b3f892708655712363562555d2aabe85145
dariNirvana <takina0721@gmail.com>
2024-04-14 01:59:10 +0800

    fix bot epub logic

--- bot/env.py
+++ bot/env.py
@@ -1,10 +1,10 @@
 import os
 
 # optional
-PROXY_URL = os.getenv('PROXY_URL', 'http://127.0.0.1:7890')
-DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', 8)
+PROXY_URL = os.getenv('PROXY_URL', str)  # eg. 'http://localhost:port' and 'http://' is required
+DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', int)  # recommend at 8-16
 # necessary
-BOT_TOKEN = os.getenv('TG_BOT_TOKEN', '6707614408:AAG6nQbYkln_lowLSC95p57eTvQbatYo32M')
-DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', "/home/tairitsucat/Templates/")
-USER_ID = os.getenv('USER_ID', 5229239723)
+BOT_TOKEN = os.getenv('TG_BOT_TOKEN', str)  # get from bot father
+DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', str)
+USER_ID = os.getenv('USER_ID', int)
 
