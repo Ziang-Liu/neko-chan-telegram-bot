3025630a121437afc22958db2c3d49f0910a3c63
dariNirvana <takina0721@gmail.com>
2024-04-14 11:59:55 +0800

    reliability improvement

--- bot/env.py
+++ bot/env.py
@@ -1,10 +1,10 @@
 import os
 
 # optional
-PROXY_URL = os.getenv('PROXY_URL', 'http://127.0.0.1:7890')
-DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', 8)
+PROXY_URL = os.getenv('PROXY_URL', None)  # eg. 'http://localhost:port' and 'http://' is required
+DOWNLOAD_THREADS = os.getenv('DOWNLOAD_THREADS', 4)  # recommend at 8-16
 # necessary
-BOT_TOKEN = os.getenv('TG_BOT_TOKEN', '6707614408:AAG6nQbYkln_lowLSC95p57eTvQbatYo32M')
-DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', "/home/tairitsucat/Templates/")
-USER_ID = os.getenv('USER_ID', 5229239723)
-
+BOT_TOKEN = os.getenv('BOT_TOKEN', '')  # get from bot father
+USER_ID = os.getenv('USER_ID', -1)
+# other
+DOWNLOAD_PATH = os.getenv('DOWNLOAD_PATH', '/download')  # you should mount the path
