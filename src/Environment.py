24bac394310bb9998e7c10419b6baef5a68c87e4
dariNirvana <takina0721@gmail.com>
2024-06-07 20:35:36 +0800

    :sparkles: add new env variables

--- src/Environment.py
+++ src/Environment.py
@@ -5,44 +5,31 @@ from src.utils.Logger import logger
 
 class EnvironmentReader:
     def __init__(self):
-        # bot params
-        self.BOT_TOKEN: str | None = os.getenv('BOT_TOKEN', None)  # required
-        self.MY_USER_ID: int | None = os.getenv('MY_USER_ID', None)  # in common sense, it is required
-        self.BASE_URL: str = os.getenv('BASE_URL', 'https://api.telegram.org/bot')
-        self.BASE_FILE_URL: str = os.getenv('BASE_FILE_URL', 'https://api.telegram.org/file/bot')
-
-        # basic working dirs, no need to change
-        self.BASE_DIR = os.getenv('BASE_DIR', '/media')
-        os.makedirs(name = self.BASE_DIR, exist_ok = True, mode = 0o777)
-        self.KOMGA_PATH = os.getenv('KOMGA_PATH', f'{self.BASE_DIR}/komga/')
-        self.DMZJ_PATH = os.getenv('DMZJ_PATH', f'{self.BASE_DIR}/dmzj/')
-        self.EPUB_PATH = os.getenv('EPUB_PATH', f'{self.BASE_DIR}/epub/')
-        self.TEMP_PATH = os.getenv('TEMP_PATH', f'{self.BASE_DIR}/.temp/')
-
-        # specific params
-        self.PROXY: None | str = os.getenv('PROXY', None)  # if your country has GFW, it is required
-        self.TELEGRAPH_THREADS = int(os.getenv('TELEGRAPH_THREADS', 4))  # optional
+        self.BOT_TOKEN = os.getenv('BOT_TOKEN', None)
+        self.CHAT_ANYWHERE_KEY = os.getenv('CHAT_ANYWHERE_KEY', None)
+        self.MY_USER_ID = int(os.getenv('MY_USER_ID', -1))
+        self.BASE_URL = os.getenv('BASE_URL', 'https://api.telegram.org/bot')
+        self.BASE_FILE_URL = os.getenv('BASE_FILE_URL', 'https://api.telegram.org/file/bot')
+        self.PROXY = os.getenv('PROXY', None)
+        self.TELEGRAPH_THREADS = int(os.getenv('TELEGRAPH_THREADS', 4))
 
     def print_env(self):
-        logger.info("---------------Environment variables---------------")
-        logger.info(f"Master's user id: {self.MY_USER_ID}")
-        logger.info(f"Proxy: {self.PROXY}")
-        logger.info(f"Telegraph threads: {self.TELEGRAPH_THREADS}")
-        logger.info(f"Base URL: {self.BASE_URL}")
-        logger.info(f"Base File URL: {self.BASE_FILE_URL}")
-        logger.info("---------------------------------------------------")
+        logger.info(f"[Env]: Master user id: {self.MY_USER_ID}")
+        logger.info(f"[Env]: Proxy: {self.PROXY}")
+        logger.info(f"[Env]: Telegraph threads: {self.TELEGRAPH_THREADS}")
+        logger.info(f"[Env]: Base URL: {self.BASE_URL}")
+        logger.info(f"[Env]: Base File URL: {self.BASE_FILE_URL}")
 
     def print_attribute(self, attribute_name):
         if hasattr(self, attribute_name):
             attribute_value = getattr(self, attribute_name)
-            logger.info(f"{attribute_name}: {attribute_value}")
+            logger.info(f"[Env]: {attribute_name}: {attribute_value}")
         else:
             attribute_value = getattr(self, attribute_name)
-            logger.info(f"{attribute_name}: {attribute_value}")
+            logger.info(f"[Env]: {attribute_name}: {attribute_value}")
 
     def get_variable(self, variable_name):
         value = getattr(self, variable_name, None)
-
         if isinstance(value, str):
             return value.strip() if value is not None else None
 
