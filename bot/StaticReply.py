a7b602f5b189c3cb2ff0ff613869b8f03d81fc9c
dariNirvana <takina0721@gmail.com>
2024-06-18 19:16:08 +0800

    :memo: update /help

--- bot/StaticReply.py
+++ bot/StaticReply.py
@@ -13,14 +13,16 @@ async def introduce(update: Update, _):
 async def instructions(update: Update, _):
     await update.message.reply_markdown(
         f"_Command List_:\n\n"
-        f"/hug , /pet , /kiss , /cuddle , /snog :"
-        f"只是对待猫猫的不同手段而已（搂搂抱抱随意选择），会根据你左滑消息引用的内容自动匹配出你想要什么"
-        f"(filters.PHOTO: 搜图模式，filters.Document.IMAGE: 搜图模式，"
-        f"filters.Sticker.ALL: 贴纸下载，message.text 包含 Telegraph 漫画: 上传 epub，message.text 包含链接：尝试链接搜图)\n"
-        f"/komga [Auto close: 5min]:"
-        f"仅限于所有者填写环境变量中的个人ID后启用，使用命令后将 Telegraph 漫画交给 Neko 就好，她会帮你妥善整理在服务器里的 c:\n"
-        f"/chat [Fallback: /bye, Auto close: 5min]:"
-        f"支持 ChatAnywhere API 的 GPT 模式，和 Neko 愉快交流吧！(免费令牌会时不时出现请求错误，后面考虑换一个接口)\n"
+        f"/hug | /pet | /kiss | /cuddle | /snog \n"
+        f"对待猫猫的不同手段（搂搂抱抱随意选择），Neko 会根据你回复的内容自动自动选择功能:\n"
+        f"`功能列表: (PHOTO | Document.IMAGE | Preview.URL: 搜图, "
+        f"Sticker: 贴纸下载，text 包含 Telegraph 漫画: 上传 epub)`\n\n"
+        f"/anime \n"
+        f"支持通过回复文件上传或者聊天图片（番剧截图）来进行番剧的时间线搜索\n\n"
+        f"/komga \n`Auto close: 5min` \n"
+        f"仅限于所有者填写环境变量中的个人ID后启用，使用命令后将 Telegraph 漫画交给 Neko 就好，她会帮你妥善整理在服务器里的 c:\n\n"
+        f"/chat \n`Fallback:` /bye  `Auto close: 5min` \n"
+        f"支持 ChatAnywhere API Token，和 Neko 愉快交流吧！(免费令牌会时不时出现请求错误，后面考虑换一个接口)\n"
     )
 
 
