ab414a3a89b8929eabb06e3deffa7f036f00fc8f
dariNirvana <takina0721@gmail.com>
2024-07-28 23:19:58 +0800

    :sparkles: remove telegram base url env and add cloudflare worker proxy url env

--- src/Environment.py
+++ src/Environment.py
@@ -8,17 +8,15 @@ class EnvironmentReader:
         self.BOT_TOKEN = os.getenv('BOT_TOKEN', None)
         self.CHAT_ANYWHERE_KEY = os.getenv('CHAT_ANYWHERE_KEY', None)
         self.MY_USER_ID = int(os.getenv('MY_USER_ID', -1))
-        self.BASE_URL = os.getenv('BASE_URL', 'https://api.telegram.org/bot')
-        self.BASE_FILE_URL = os.getenv('BASE_FILE_URL', 'https://api.telegram.org/file/bot')
         self.PROXY = os.getenv('PROXY', None)
-        self.TELEGRAPH_THREADS = int(os.getenv('TELEGRAPH_THREADS', 4))
+        self.CF_WORKER_PROXY = os.getenv('CF_WORKER_URL', None)
+        self.TELEGRAPH_THREADS = int(os.getenv('TELEGRAPH_THREADS', 2))
 
     def print_env(self):
-        logger.info(f"[Env]: Master user id: {self.MY_USER_ID}")
-        logger.info(f"[Env]: Proxy: {self.PROXY}")
+        logger.info(f"[Env]: Master user id: {self.MY_USER_ID}") if self.MY_USER_ID is not -1 else None
         logger.info(f"[Env]: Telegraph threads: {self.TELEGRAPH_THREADS}")
-        logger.info(f"[Env]: Base URL: {self.BASE_URL}")
-        logger.info(f"[Env]: Base File URL: {self.BASE_FILE_URL}")
+        logger.info(f"[Env]: Proxy: {self.PROXY}") if not self.PROXY else None
+        logger.info(f"[Env]: CloudFlare Worker Proxy: {self.CF_WORKER_PROXY}") if not self.CF_WORKER_PROXY else None
 
     def print_attribute(self, attribute_name):
         if hasattr(self, attribute_name):
